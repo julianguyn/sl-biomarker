a2b4d6e87fd415946fc8948678e3a2dec156b656
Julia Nguyen <julianguyenpt@gmail.com>
2025-11-27 11:44:31 -0500

    fix: cluster resources

---
+++ snake_submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=get_SL_scores
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jlm.nguyen@mail.utoronto.ca
+
+conda init --all
+conda activate synleth
+
+snakemake \
+  -s workflow/Snakefile \
+  --latency-wait 60 \
+  --keep-going \
+  --cluster "sbatch \
+      -p {resources.partition} \
+      --mem={resources.mem} \
+      -t {resources.time} \
+      -c {resources.cpus} \
+      -o logs/{rule}.{wildcards}.out \
+      -J {rule}" \
+  -j 40 \
+  -F
