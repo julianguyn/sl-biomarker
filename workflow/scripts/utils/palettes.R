9c95877325329d165ca42d876bbb5aa6a0e142a6
Julia Nguyen <julianguyenpt@gmail.com>
2026-01-31 15:38:22 -0500

    feat: heatmap of mutation class

---
+++ workflow/scripts/utils/palettes.R
@@ -0,0 +1,29 @@
+binary_pal <- c(
+    "#073062",
+    "white"
+)
+
+binary_pal2 <- c(
+    "#93A5B6",
+    "white"
+)
+
+amplicon_class_pal <- c(
+    "BFB" = "#CC9979",
+    "Complex-non-cyclic" = "#ECB8A5",
+    "Linear" = "#ECCFC3",
+    "ecDNA" = "#957D95"
+)
+
+variant_pal <- c(
+    "SNP" = "#9CADCE",
+    "DEL" = "#BB8F87",
+    "INS" = "#D4C9B0",
+    "DNP" = "#7A90BB",
+    "TNP" = "#5A6E98",
+    "SNP & DEL" = "#6C698D",
+    "SNP & INS" = "#96B0A2",
+    "SNP & DNP" = "#4D586E",
+    "ING & DEL" = "#82645F",
+    "INS & DEL" = "#AA968A"
+)
