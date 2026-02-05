021f57cd46167d1d916e18c7d8d4702ad2d38dc4
Julia Nguyen <julianguyenpt@gmail.com>
2026-02-05 18:07:56 -0500

    feat: validate SKOs in ccls

--- workflow/scripts/4-CCL-validation/top_SKOs.R
+++ workflow/scripts/4-CCL-validation/top_SKOs.R
@@ -1,6 +1,5 @@
 # load libraries
 suppressPackageStartupMessages({
-    library(PharmacoGx)
     library(tidyverse)
     library(ggplot2)
     library(data.table)
@@ -26,102 +25,147 @@ targets$Drug <- str_to_sentence(targets$Drug)
 load("data/procdata/psets/sensitivity_data.RData")
 
 ###########################################################
-# TODO:: Turn this into function:
+# Validate SKOs in each PSet
 ###########################################################
 
-# args:
-filepath <- "data/procdata/psets/CCLE_RSEM_TPM_reheadered.tsv"
-stat_options <- c('1st Qu.', 'Median', 'thirds')
-drug <- "Crizotinib"
-stat <- stat_options[1]
-sen <- ctrp_sen
-pset <- "CTRP2"
+validate_SKOs <- function(rna, sen, stat, pset) {
 
-# load in rna
-rna <- fread(filepath, data.table = FALSE)
-rownames(rna) <- rna$Hugo_Symbol
-rna$Hugo_Symbol <- NULL
+    # load in rna
+    filepath <- paste0("data/procdata/psets/", rna, "_RSEM_TPM_reheadered.tsv")
+    rna <- fread(filepath, data.table = FALSE)
+    rownames(rna) <- rna$Hugo_Symbol
+    rna$Hugo_Symbol <- NULL
 
-
-for (i in 1:nrow(top_SKO_tar)) {
-
-    # get SKO pair
-    gm <- top_SKO_tar$mutated_gene[i]
-    gu <- top_SKO_tar$unmutated_gene[i]
-
-    # get targeted therapies
-    gu_targ <- targets$Drug[targets$Gene == gu]
-    sen_targ <- sen[rownames(sen) %in% gu_targ,]
-    if (nrow(sen_targ) == 0) next
-    sen_targ <- t(sen_targ) |> as.data.frame()
-    
-    # subset for SKO pair
-    sub <- rna[rownames(rna) %in% c(gm, gu),] |>
-        t() |> as.data.frame()
-    
-    # get summary stats
-    stats <- data.frame(
-        Mutated = as.numeric(summary(sub[[gm]])),
-        Unmutated = as.numeric(summary(sub[[gu]]))
-    )
-    rownames(stats) <- names(summary(sub[[gm]]))
-    stats <- t(stats) |> as.data.frame()
-    stats$thirds <- (stats$Max - stats$Min)/3
-
-    # use selected stat to annotate KO
-    sub[[gm]] <- ifelse(sub[[gm]] <= stats[[stat]][rownames(stats) == "Mutated"], 0, 1)
-    sub[[gu]] <- ifelse(sub[[gu]] <= stats[[stat]][rownames(stats) == "Unmutated"], 0, 1)
-
-    # label SKO
-    sub <- sub[complete.cases(sub),]
-    sub$status <- NA
-    for (i in 1:nrow(sub)) {
-        if (sub[[gm]][i] == 0 & sub[[gu]][i] == 0) sub$status[i] <- "DKO"
-        if (sub[[gm]][i] == 1 & sub[[gu]][i] == 0) sub$status[i] <- "SKO_targetable"
-        if (sub[[gm]][i] == 0 & sub[[gu]][i] == 1) sub$status[i] <- "SKO_other"
-        if (sub[[gm]][i] == 1 & sub[[gu]][i] == 1) sub$status[i] <- "noKO"
-    }
-    sub$status <- factor(sub$status, levels = names(status_pal))
-
-    # make dataframe of common cells
-    common_cells <- intersect(rownames(sub), rownames(sen_targ))
-    df <- data.frame(
-        sample = common_cells,
-        SL = sub$status[match(common_cells, rownames(sub))]
+    # initiate dataframe
+    results <- data.frame(matrix(nrow=0, ncol=9))
+    colnames(results) <- c(
+        "PSet", "Gene_m", "Gene_u", "Drug", "Stat",
+        "p.sko1", "p.sko2", "p.dko1", "p.dko2"
     )
 
-    # iterate for every available drug
-    for (drug in rownames(sen_targ)) {
-        df$sensitivity <- sen_targ[[drug]][match(common_cells, rownames(sen_targ))]
+    # get stat_dir
+    stat_dir <- stat
+    if (stat == "1st Qu.") stat_dir <- "quartile1"
 
-        # plot boxplots
-        p <- ggplot(df, aes(x = SL, y = sensitivity, fill = SL)) +
-        geom_boxplot() + geom_jitter(width = 0.2, alpha = 0.5) +
-        scale_fill_manual("Synthetic Lethal\nInteraction", values = SL_pal) +
-        theme_minimal() +
-        theme(panel.border = element_rect()) +
-        labs(y = paste(drug, "Response (AAC)"), x = "Synthetic Lethal Interaction") +
-        ggtitle(paste0("Dataset: ", pset, ", SKO: ", gm, " & ", gu))
+    for (i in 1:nrow(top_SKO_tar)) {
+
+        print(i)
+
+        # get SKO pair
+        gm <- top_SKO_tar$mutated_gene[i]
+        gu <- top_SKO_tar$unmutated_gene[i]
+
+        # get targeted therapies
+        gu_targ <- targets$Drug[targets$Gene == gu]
+        sen_targ <- sen[rownames(sen) %in% gu_targ,]
+        if (nrow(sen_targ) == 0) next
+        sen_targ <- t(sen_targ) |> as.data.frame()
         
-        filename <- paste0("data/results/figures/CCL-top-SKOs/", pset, "_", gm, "-", gu, "_", drug, ".png")
-        png(filename, width = 6, height = 4.5, res = 600, units = "in")
-        print(p)
-        dev.off()
+        # subset for SKO pair
+        sub <- rna[rownames(rna) %in% c(gm, gu),] |>
+            t() |> as.data.frame()
+        
+        # get summary stats
+        stats <- data.frame(
+            Mutated = as.numeric(summary(sub[[gm]])),
+            Unmutated = as.numeric(summary(sub[[gu]]))
+        )
+        rownames(stats) <- names(summary(sub[[gm]]))
+        stats <- t(stats) |> as.data.frame()
+        stats$thirds <- (stats$Max - stats$Min)/3
 
-        # perform t-tests
-        # todo: save results to dataframe per pset
+        # use selected stat to annotate KO
+        sub[[gm]] <- ifelse(sub[[gm]] <= stats[[stat]][rownames(stats) == "Mutated"], 0, 1)
+        sub[[gu]] <- ifelse(sub[[gu]] <= stats[[stat]][rownames(stats) == "Unmutated"], 0, 1)
 
+        # label SKO
+        sub <- sub[complete.cases(sub),]
+        sub$status <- NA
+        for (i in 1:nrow(sub)) {
+            if (sub[[gm]][i] == 0 & sub[[gu]][i] == 0) sub$status[i] <- "DKO"
+            if (sub[[gm]][i] == 1 & sub[[gu]][i] == 0) sub$status[i] <- "SKO_targetable"
+            if (sub[[gm]][i] == 0 & sub[[gu]][i] == 1) sub$status[i] <- "SKO_other"
+            if (sub[[gm]][i] == 1 & sub[[gu]][i] == 1) sub$status[i] <- "noKO"
+        }
+        sub$status <- factor(sub$status, levels = names(SL_pal))
+
+        # make dataframe of common cells
+        common_cells <- intersect(rownames(sub), rownames(sen_targ))
+        df <- data.frame(
+            sample = common_cells,
+            SL = sub$status[match(common_cells, rownames(sub))]
+        )
+        if (length(df$SL[df$SL == "SKO_targetable"]) < 2) next
+
+        # iterate for every available drug
+        for (drug in colnames(sen_targ)) {
+            print(paste("--------", drug))
+            df$sensitivity <- sen_targ[[drug]][match(common_cells, rownames(sen_targ))]
+            toPlot <- df[complete.cases(df),]
+            if (length(toPlot$SL[toPlot$SL == "SKO_targetable"]) < 2) next
+
+            # plot boxplots
+            p <- ggplot(toPlot, aes(x = SL, y = sensitivity, fill = SL)) +
+            geom_boxplot() + geom_jitter(width = 0.2, alpha = 0.5) +
+            scale_fill_manual("Synthetic Lethal\nInteraction", values = SL_pal) +
+            theme_minimal() +
+            theme(panel.border = element_rect()) +
+            labs(y = paste(drug, "Response (AAC)"), x = "Synthetic Lethal Interaction") +
+            ggtitle(paste0("Dataset: ", pset, ", SKO: ", gm, " & ", gu))
+            
+            filename <- paste0("data/results/figures/CCL-top-SKOs/", stat_dir, "/", pset, "/", gm, "-", gu, "_", drug, ".png")
+            png(filename, width = 6, height = 4.5, res = 600, units = "in")
+            print(p)
+            dev.off()
+
+            # get SL classes
+            sko_t <- toPlot$sensitivity[toPlot$SL == "SKO_targetable"]
+            sko_o <- toPlot$sensitivity[toPlot$SL == "SKO_other"]
+            dko <- toPlot$sensitivity[toPlot$SL == "DKO"]
+            noko <- toPlot$sensitivity[toPlot$SL == "noKO"]
+
+            # perform t-tests
+            sko1 <- ifelse(
+                length(sko_t) > 3 & length(sko_o) > 3,
+                t.test(sko_t, sko_o, alternative = "greater")$p.value,
+                NA
+            )
+            sko2 <- ifelse(
+                length(sko_t) > 3 & length(noko) > 3,
+                t.test(sko_t, noko, alternative = "greater")$p.value,
+                NA
+            )
+            dko1 <- ifelse(
+                length(dko) > 3 & length(noko) > 3,
+                t.test(dko, noko, alternative = "greater")$p.value,
+                NA
+            )
+            dko2 <- ifelse(
+                length(dko) > 3 & length(sko_o) > 3,
+                t.test(dko, sko_o, alternative = "greater")$p.value,
+                NA
+            )
+        
+            row <- data.frame(
+                PSet = pset, Gene_m = gm, Gene_u = gu, 
+                Drug = drug, Stat = stat, 
+                p.sko1 = sko1, p.sko2 = sko2, 
+                p.dko1 = dko1, p.dko2 = dko2
+            )
+            results <- rbind(results, row)
+        }
     }
+    results <- results[order(results$p.sko1, results$p.dko1),]
+    filepath <- paste0("data/results/data/CCL-top-SKOs/", stat_dir, "/", pset, ".csv")
+    write.csv(results, file = filepath, quote = FALSE, row.names = FALSE)
 
+    return(results)
 }
 
-###########################################################
-# ttest TODO
-###########################################################
+stat_options <- c("1st Qu.", "Median", "thirds")
 
-
-
-dko <- sen$Lapatinib[rownames(sen) %in% rownames(sub[sub$status == "DKO",])]
-sko <- sen$Lapatinib[rownames(sen) %in% rownames(sub[sub$status != "DKO",])]
-t.test(dko, sko, alternative = "two.sided")
-
+s = 3
+ccle <- validate_SKOs("CCLE", ctrp_sen, stat_options[s], "CTRP")
+gdsc <- validate_SKOs("GDSC", gdsc_sen, stat_options[s], "GDSC")
+gcsi <- validate_SKOs("gCSI", gcsi_sen, stat_options[s], "gCSI")
+gray <- validate_SKOs("GRAY", gray_sen, stat_options[s], "GRAY")
