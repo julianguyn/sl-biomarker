ff6242b65a321085a371dfca38eddcc0672ded54
Julia Nguyen <julianguyenpt@gmail.com>
2026-05-29 16:29:29 -0400

    feat: process PM RNA files

--- workflow/scripts/0-data-processing/process_meta.R
+++ workflow/scripts/0-data-processing/process_meta.R
@@ -35,7 +35,8 @@ meta <- data.frame(
     Cohort = all_samples$cohort,
     Sample_ID = all_samples$sample,
     MOHCCN_ID = NA,
-    Patient_ID = sub("^([A-Za-z0-9]+_[0-9]+)_.*$", "\\1", all_samples$sample)
+    Patient_ID = sub("^([A-Za-z0-9]+_[0-9]+)_.*$", "\\1", all_samples$sample),
+    PatCan_ID = sub("^([A-Za-z0-9]+_[0-9]+_[A-Za-z]+_[A-Z])_.*$", "\\1", all_samples$sample),
     Batch = all_samples$batch,
     Duplicated = all_samples$duplicated,
     Results_Table = all_samples$result_table
@@ -59,7 +60,7 @@ PM_meta <- meta[meta$Centre == "PM2C",]
 BC_meta <- meta[meta$Centre == "BCCA",]
 
 ###########################################################
-# Map RNA to PM
+# Create metadata of available RSEM
 ###########################################################
 
 load("metadata/PM2C_RSEM.RData")
@@ -73,17 +74,125 @@ rsem_meta <- data.frame(
     Omics_ID = sub("\\.RSEM.*", "", c(batch1, batch2))
 )
 rsem_meta$Patient_ID <- sub("^([A-Za-z0-9]+_[0-9]+)_.*$", "\\1", rsem_meta$Omics_ID)
-
+rsem_meta$PatCan_ID <- sub("^([A-Za-z0-9]+_[0-9]+_[A-Za-z]+_[A-Z])_.*$", "\\1", rsem_meta$Omics_ID)
 
 # 1690 unique sample IDs from PM_meta
-# 451 from batch1, 1118 from batch2 = 1569 total
+# 461 from batch1, 1118 from batch2 = 1579 total
 
-# trying to map the IDs 
+table(rsem_meta$Patient_ID %in% PM_meta$Patient_ID) #all 1579 match
 
-table(rsem_meta$Omics_ID %in% PM_meta$Sample_ID)
+###########################################################
+# Check duplicates
+###########################################################
 
+# one duplicate in the PM_meta patient IDs (P and M)
+duplicated_PM_id <- PM_meta$Patient_ID[duplicated(PM_meta$Patient_ID)]
 
-table(sub("_WT_", "_WG_", batch1) %in% PM_meta$Sample_ID)
+# 58 duplicates in the RSEM
+duplicated_PM_rna <- rsem_meta$Patient_ID[duplicated(rsem_meta$Patient_ID)]
+
+# ------ check duplicates
+tt <- rsem_meta[rsem_meta$Patient_ID %in% duplicated_PM_rna,]
+tt <- tt[order(tt$Patient_ID),]
+
+b1 <- tt[tt$Batch == "Batch1",] #45
+b2 <- tt[tt$Batch == "Batch2",] #70
+
+# 45 are duplicates in the batches (they have the same Omics_ID)
+two_batches <- duplicated_PM_rna[which(duplicated_PM_rna %in% b1$Patient_ID & duplicated_PM_rna %in% b2$Patient_ID)]
+table(b1$Omics_ID[b1$Patient_ID %in% two_batches] == b2$Omics_ID[b2$Patient_ID %in% two_batches])
+
+# remove these duplicates
+rsem_meta <- rsem_meta[-which(rsem_meta$Patient_ID %in% two_batches & rsem_meta$Batch == "Batch2"),]
+# 1534 left in rsem_meta
+
+# remaining duplicates are all in b2
+b2 <- b2[-which(b2$Patient_ID %in% two_batches),] #25, 12 unique PatientIDs
+
+###########################################################
+# Try mapping to the ecDNA IDs
+###########################################################
+
+rsem_meta$match_WG <- sub("_WT", "_WG", sub("RNA", "DNA", rsem_meta$Omics_ID)) # these are all unique
+table(rsem_meta$match_WG %in% PM_meta$Sample_ID) # 1472 match, 62 don't match
+
+###### check the ones that don't match
+no_match <- rsem_meta[-which(rsem_meta$match_WG %in% PM_meta$Sample_ID),] #62
+dup <- no_match$Patient_ID[duplicated(no_match$Patient_ID)] # 1 duplicate
+
+table(no_match$Patient_ID %in% PM_meta$Patient_ID) # all true lol
+
+no_match$Matched_ID <- NA
+for (sample in no_match$Patient_ID) {
+
+    match <- PM_meta[PM_meta$Patient_ID == sample,]
+    if (nrow(match) == 1) {
+        no_match$Matched_ID[no_match$Patient_ID == sample] <- match$Sample_ID
+    } else {
+        cat("----- Sample:", sample, "\n", nrow(match))
+    }
+}
+
+#### check the ones that do match
+matched <- rsem_meta[rsem_meta$match_WG %in% PM_meta$Sample_ID,] #1472
+
+# check if any of these had an indirect match wtih a B2 dup
+to_check <- no_match[no_match$Patient_ID %in% unique(b2$Patient_ID),] # 13 cases here
+to_check[to_check$Matched_ID %in% matched$match_WG,] |> dim() # all 13 had a better direct match
+
+b2$removed <- ifelse(b2$Omics_ID %in% to_check$Omics_ID, "Removed", "Kept")
+no_match <- no_match[-which(no_match$Omics_ID %in% to_check$Omics_ID),] # 49 manually mapped remaining
+
+tt <- no_match[,c(2,5,6)] 
+write.csv(tt, file = "metadata/manual_rna_ecdna_match.csv", quote = FALSE, row.names = FALSE)
+
+# merge back (1521 samples matched to RNA)
+length(unique(matched$Omics_ID))
+length(matched$Omics_ID)
+
+length(unique(no_match$Omics_ID))
+length(no_match$Omics_ID)
+
+length(unique(c(matched$Omics_ID, no_match$Omics_ID)))
+
+###########################################################
+# Clean up RSEM Meta
+###########################################################s
+ 
+# 1521 matches (1472 direct, 49 indirect), removed 13 duplicates
+rsem_meta$Status <- ifelse(
+    rsem_meta$Omics_ID %in% matched$Omics_ID, "Direct Match",
+    ifelse(
+        rsem_meta$Omics_ID %in% to_check$Omics_ID, "Removed (Dup)", "Indirect Match"
+    )
+)
+
+for (i in 1:nrow(rsem_meta)) {
+    sample <- rsem_meta$Omics_ID[i]
+    if (rsem_meta$Status[i] == "Indirect Match") {
+        rsem_meta$match_WG[i] <- no_match$Matched_ID[no_match$Omics_ID == sample]
+    } else if (rsem_meta$Status[i] == "Removed (Dup)") {
+        rsem_meta$match_WG[i] <- "Removed"
+    }
+}
+table(rsem_meta$match_WG %in% PM_meta$Sample_ID) # 1521 match, 13 dups
+
+###########################################################
+# Add RNA info to PM_meta
+###########################################################s
+ 
+PM_meta$RNA <- "missing"
+for (sample in PM_meta$Sample_ID) {
+    if (sample %in% rsem_meta$match_WG) {
+        PM_meta$RNA[PM_meta$Sample_ID == sample] <- rsem_meta$Omics_ID[rsem_meta$match_WG == sample]
+    }
+}
+dim(PM_meta[PM_meta$RNA == "missing",]) #169 cases with missing RNA
+
+write.csv(PM_meta, file = "metadata/2026-05-29_PM_meta.csv", quote = FALSE, row.names = FALSE)
+
+missing_RNA <- PM_meta[PM_meta$RNA == "missing",]
+write.csv(missing_RNA, file = "metadata/2026-05-29_PM_meta_missing_RNA.csv", quote = FALSE, row.names = FALSE)
 
 ###########################################################
 # Mapping omics to ecDNA
